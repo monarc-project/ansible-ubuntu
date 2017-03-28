0c3af6ac347e586a8675a887b225f705532d290e
Xavier Claude <xavier.claude@conostix.com>
2017-03-28 09:18:53 +0200

    initial commit

---
+++ add_inventory.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""
+Add an attribute for the ansible inventory
+"""
+
+import json
+import random
+import string
+
+import sys
+import yaml
+
+INVENTORY = '/home/xavier/clients/smile/monarcv2/inventory/'
+
+#{"server":"client4.prod.dims.lc1.conostix.com","client":"grclux","salt":"zG@0q1EI",
+#"jabber_account":"grclux@jabber.cases.lu","sql_bootstrap":
+
+def get_rnd_string(length):
+    """Get random string"""
+    return ''.join(random.choice(
+        string.ascii_uppercase + string.digits) for _ in range(length))
+
+def run():
+    """ Main function """
+    newdata = json.loads(sys.stdin.read())
+
+    with open('%s/host_vars/%s/generated.yaml' % (INVENTORY, newdata['server']), 'r+') as stream:
+        try:
+            ymldata = yaml.load(stream)
+            client_list = ymldata['clients']
+            client_name = newdata['client']
+            client_list[client_name] = {}
+            client_list[client_name]['name'] = client_name
+            client_list[client_name]['mysql_password'] = get_rnd_string(16)
+            client_list[client_name]['salt'] = get_rnd_string(64)
+
+            yaml.dump(stream)
+        except yaml.YAMLError as exc:
+            print exc
+            exit(1)
+
+run()
