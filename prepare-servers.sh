f9ee3440f947aef8040707c6894f3ab0e21a4160
Cédric Bonhomme <cedric@cedricbonhomme.org>
2017-09-15 10:50:18 +0200

    minor updates to the prepare-servers.sh script

--- prepare-servers.sh
+++ prepare-servers.sh
@@ -4,22 +4,28 @@
 #
 # ./prepare-server.sh IPrpx IPbo IPfo1 <host_username> <ansible_username> <ansible_email> <proxy_URL>
 
+
+# To locales problems on the server
+#  export LC_ALL="en_US.UTF-8"
+#  export LC_CTYPE="en_US.UTF-8"
+#  sudo dpkg-reconfigure locales
+#
+
 IP_RPX=$1
 IP_BO=$2
 IP_FO1=$3
 HOST_USER_NAME=${4:-cases}
 ANSIBLE_USER_NAME=${5:-ansible}
 ANSIBLE_USER_PASSWORD="$(openssl rand -hex 32)"
+#echo $ANSIBLE_USER_PASSWORD
 ANSIBLE_USER_EMAIL=${6:-'info@cases.lu'}
 PROXY=${7:-''}
 
 sudo apt-get install -y python python-pip git
-sudo -H pip install ansible dnspython
 
 # Create a user for ansible
 sudo useradd --create-home -s /bin/bash $ANSIBLE_USER_NAME
 echo "$ANSIBLE_USER_NAME:$ANSIBLE_USER_PASSWORD" | sudo chpasswd
-echo $ANSIBLE_USER_PASSWORD
 
 # Generate a SSH key for the new user
 sudo -u $ANSIBLE_USER_NAME ssh-keygen -t rsa -C "$ANSIBLE_USER_EMAIL"
@@ -30,6 +36,8 @@ if [ "$PROXY" != '' ]; then
     echo "export https_proxy=https://$PROXY" >> /home/$ANSIBLE_USER_NAME/.bashrc
 fi
 
+sudo -EH pip install --upgrade pip setuptools
+sudo -EH pip install ansible dnspython
 
 for ip in IP_RPX IP_BO IP_FO1; do
     echo "Configuring $ip..."
@@ -46,8 +54,5 @@ for ip in IP_RPX IP_BO IP_FO1; do
         ssh $ANSIBLE_USER_NAME@$ip -t " echo 'export http_proxy=http://$PROXY' >> /home/$ANSIBLE_USER_NAME/.bashrc  ;  echo 'export https_proxy=https://$PROXY' >> /home/$ANSIBLE_USER_NAME/.bashrc"
     fi
 done
-
-# return to HOST_USER_NAME user
-exit
 
 exit 0
