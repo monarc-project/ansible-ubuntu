249c94741a96ed4d7c5f0abc7602b0203cf13af2
Cédric Bonhomme <cedric@cedricbonhomme.org>
2017-10-03 10:06:40 +0200

    update scripts

--- playbook/monarcbo/files/del_monarc_clients.sh
+++ playbook/monarcbo/files/del_monarc_clients.sh
@@ -2,12 +2,9 @@
 
 spool_dir="/var/lib/monarc/bo/MonarcAppBO/data/json/delete/"
 
-cat "$spool_dir"*.json
-mv "$spool_dir"*.json /var/tmp/monarc/deleted/
-
-# if [ "$(ls -A $spool_dir)" ] ; then
-# 	for file in "$spool_dir"/* ; do
-# 		cat "$file"
-# 		mv "$file" /var/tmp/monarc/deleted/
-# 	done
-# fi
+if [ "$(ls -A $spool_dir)" ] ; then
+	for file in "$spool_dir"/* ; do
+		cat "$file"
+		mv "$file" /var/tmp/monarc/deleted/
+	done
+fi
--- playbook/monarcbo/files/new_monarc_clients.sh
+++ playbook/monarcbo/files/new_monarc_clients.sh
@@ -2,12 +2,9 @@
 
 spool_dir="/var/lib/monarc/bo/MonarcAppBO/data/json/create/"
 
-cat "$spool_dir"*.json
-mv "$spool_dir"*.json /var/tmp/monarc/created/
-
-# if [ "$(ls -A $spool_dir)" ] ; then
-# 	for file in "$spool_dir"/* ; do
-# 		cat "$file"
-# 		mv "$file" /var/tmp/monarc/created/
-# 	done
-# fi
+if [ "$(ls -A $spool_dir)" ] ; then
+	for file in "$spool_dir"/* ; do
+		cat "$file"
+		mv "$file" /var/tmp/monarc/created/
+	done
+fi
