1fdc47fe3745b7d0b0b5f320f282bbbbf4893a06
Cédric Bonhomme <cedric@cedricbonhomme.org>
2017-10-03 10:30:38 +0200

    fix update.sh script

--- playbook/update.sh
+++ playbook/update.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Launch the ansible playbook when necessary.
 # Launch this file with cron (as the user 'ansible')
@@ -9,13 +9,13 @@ BO=$2
 cd $PLAYBOOK_PATH
 
 RESULT=`ssh ansible@$BO sudo -u www-data /usr/local/bin/new_monarc_clients.sh | jq -s . | ./add_inventory.py ../inventory/`
-if [ $RESULT -eq 0 ]; then
+if [ "$RESULT" = "0" ]; then
     echo 'New clients detected. Running ansible-playbook...'
     ansible-playbook -i ../inventory/ monarc.yaml --user ansible
 fi
 
 RESULT=`ssh ansible@$BO sudo -u www-data /usr/local/bin/del_monarc_clients.sh | jq -s . | ./del_inventory.py ../inventory/`
-if [ $RESULT -eq 0 ]; then
+if [ "$RESULT" = "0" ]; then
     echo 'Client deleted. Running ansible-playbook...'
     ansible-playbook -i ../inventory/ monarc.yaml --user ansible
 fi
