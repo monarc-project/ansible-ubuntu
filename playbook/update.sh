a16651c5562f31bf7e61f6dbf13506cb053a0fc1
Cédric Bonhomme <cedric@cedricbonhomme.org>
2017-10-24 15:50:49 +0200

    use the full path of ansible-playbook

--- playbook/update.sh
+++ playbook/update.sh
@@ -6,16 +6,18 @@
 PLAYBOOK_PATH=$1
 BO=$2
 
+ANSIBLE_PATH="/usr/local/bin/ansible-playbook"
+
 cd $PLAYBOOK_PATH
 
 RESULT=`ssh ansible@$BO sudo -u www-data /usr/local/bin/new_monarc_clients.sh | jq -s . | ./add_inventory.py ../inventory/`
 if [ "$RESULT" = "0" ]; then
     echo 'New clients detected. Running ansible-playbook...'
-    ansible-playbook -i ../inventory/ monarc.yaml --user ansible
+    $ANSIBLE_PATH -i ../inventory/ monarc.yaml --user ansible
 fi
 
 RESULT=`ssh ansible@$BO sudo -u www-data /usr/local/bin/del_monarc_clients.sh | jq -s . | ./del_inventory.py ../inventory/`
 if [ "$RESULT" = "0" ]; then
     echo 'Client deleted. Running ansible-playbook...'
-    ansible-playbook -i ../inventory/ monarc.yaml --user ansible
+    $ANSIBLE_PATH -i ../inventory/ monarc.yaml --user ansible
 fi
