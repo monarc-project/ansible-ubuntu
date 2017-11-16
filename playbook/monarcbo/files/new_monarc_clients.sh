c84880867dde80afcd9c7d0ffe6beab367291b0c
Cédric Bonhomme <cedric@cedricbonhomme.org>
2017-11-16 12:11:55 +0100

    Fixed bug 'Invalid \escape:' after parsing og the json by jq

--- playbook/monarcbo/files/new_monarc_clients.sh
+++ playbook/monarcbo/files/new_monarc_clients.sh
@@ -5,6 +5,6 @@ spool_dir="/var/lib/monarc/bo/MonarcAppBO/data/json/create/"
 if [ -z "$(ls -A $spool_dir)" ]; then
    echo ""
 else
-   echo `jq -s '.' $spool_dir*.json`
+   cat $spool_dir*.json | jq -s '.' $spool_dir*.json
    mv $spool_dir*.json /var/tmp/monarc/created/
 fi
