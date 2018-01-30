f7106edc3b1dcf36e94781ef875e1ea125ba897c
Cédric Bonhomme <cedric@cedricbonhomme.org>
2018-01-30 08:54:44 +0100

    Improved (more safe) the way yaml files are found and parsed.

--- playbook/list_inventory.py
+++ playbook/list_inventory.py
@@ -1,29 +1,33 @@
 #!/usr/bin/env python
 
 import os
-import glob
 import sys
 import json
 import yaml
+import configparser
 
 def run(INVENTORY):
     if not os.path.exists(INVENTORY):
         print 'Folder do no exists:', INVENTORY
         exit(1)
 
-    yamls = os.path.join(INVENTORY, 'host_vars' ,'*', 'generated.yaml')
+    hosts = configparser.ConfigParser(allow_no_value=True)
+    hosts.optionxform = lambda option: option
+    hosts.read(os.path.join(INVENTORY, 'hosts'))
+    fo_servers = hosts['dev'].keys()
 
-    for generated_file in glob.glob(yamls):
-        server_name = os.path.dirname(
-                os.path.relpath("../inventory/host_vars/FO/generated.yaml",
-                                os.path.join(INVENTORY, 'host_vars')))
-        with open(generated_file, 'r') as stream:
+    for fo_server in fo_servers:
+        yaml_file = os.path.join(INVENTORY, 'host_vars' ,
+                                 fo_server, 'generated.yaml')
+        if not os.path.exists(yaml_file):
+            continue
+        with open(yaml_file, 'r') as stream:
             ymldata = yaml.load(stream)
             if ymldata is None:
                 continue
             clients_list = ymldata['clients']
             for client in clients_list:
-                print("{} {}".format(server_name, client))
+                print("{} {}".format(fo_server, client))
 
 
 if __name__ == '__main__':
