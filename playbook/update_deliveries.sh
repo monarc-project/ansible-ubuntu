c8c6cbd9b3485d37fe20b3e0e3cde8f025034996
Cédric Bonhomme <cedric@cedricbonhomme.org>
2018-04-09 11:42:43 +0200

    Rename the default templates appropriately for the front offices.

--- playbook/update_deliveries.sh
+++ playbook/update_deliveries.sh
@@ -21,6 +21,9 @@ CLIENT_NAME=$3
 # Retrieve the deliveries templates from the back office.
 rsync -az $BO_ADDRESS:$DELIVERIES_DIR $DELIVERIES_TEMP_DIR > /dev/null
 
+# Rename the files appropriately for the front offices
+find $DELIVERIES_TEMP_DIR -type f -name "*_*" -exec bash -c 'f="$1"; g="${f/*_/}"; mv -- "$f" "$g"' _ '{}' \;
+
 if [ ! $? -eq 0 ]
     then
       echo 'Failed to retrieve deliveries templates from the back office.'
