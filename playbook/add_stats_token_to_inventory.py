80591bc385ebb99beae4c9f8d74f26c30475b540
Ruslan Baidan <ruslan.baidan@securitymadein.lu>
2020-11-13 17:08:50 +0100

    Updated the setting of the api key in the add_inventory, add the initial script to update the inventory with the apiKey and generate cleints if needed.

---
+++ playbook/add_stats_token_to_inventory.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import json
+import yaml
+import secrets
+
+try:
+    import configparser as configparser
+except:
+    import ConfigParser as configparser
+
+HOSTS = configparser.ConfigParser(allow_no_value=True)
+HOSTS.optionxform = lambda option: option
+
+def run(INVENTORY):
+    if not os.path.exists(INVENTORY):
+        print 'Folder do no exists:', INVENTORY
+        exit(1)
+
+    fo_servers = []
+    try:
+        HOSTS.read(os.path.join(INVENTORY, 'hosts'))
+        fo_servers = [fo_server for fo_server, _ in HOSTS.items('dev')]
+    except Exception as e:
+        exit(1)
+
+    for fo_server in fo_servers:
+        yaml_file = os.path.join(INVENTORY, 'host_vars' ,
+                                 fo_server, 'generated.yaml')
+        if not os.path.exists(yaml_file):
+            continue
+        with open(yaml_file, 'r') as stream:
+            ymldata = yaml.load(stream)
+            if ymldata is None:
+                continue
+            clients_list = ymldata['clients']
+            for client in clients_list:
+                # check if in the client's config url and key.
+                # create a new client on StatsService side if the config vars are not set:
+                # statsToken = secrets.token_urlsafe(64)
+                # go to stats server (BO) and directory or maybe api call ?
+                # poetry run flask client_create --name {client['name']} --token {statsToken}
+                # we need to update the inventory with the 'apiKey' = {statsToken}
+                # Should be fine then :)
+
+                print("{} {}".format(fo_server, client))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        INVENTORY = sys.argv[1]
+    else:
+        INVENTORY = '../inventory/'
+    run(INVENTORY)
