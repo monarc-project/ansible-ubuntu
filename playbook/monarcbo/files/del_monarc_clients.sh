aa9b8db9c44f4c2e366ddf956b1bf71b6f0277f2
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-12-14 08:03:55 +0100

    fix: bad spool dirs in new_monarc_clients.sh and del_monarc_clients.sh

--- playbook/monarcbo/files/del_monarc_clients.sh
+++ playbook/monarcbo/files/del_monarc_clients.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-spool_dir="/home/cedric/git/MONARC/ansible-ubuntu/playbook/delete/"
+spool_dir="/var/lib/monarc/bo/MonarcAppBO/data/json/delete/"
 
 if [ -z "$(ls -A $spool_dir)" ]; then
    echo ""
 else
    echo `jq -s '.' $spool_dir*.json`
-   #mv $spool_dir*.json /var/tmp/monarc/deleted/
+   mv $spool_dir*.json /var/tmp/monarc/deleted/
 fi
