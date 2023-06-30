0654f9dec8c91ed2cd3d2dc288db4e6f538e6b0e
Ruslan Baidan <ruslan.baidan@nc3.lu>
2023-06-30 11:04:07 +0200

    Added the process to automatically remove the clients' databases and user, also dedicated floder in case if the client's instance is removed.

--- playbook/del_inventory.py
+++ playbook/del_inventory.py
@@ -35,11 +35,27 @@ def run():
             path = os.path.join(
                 os.path.abspath(INVENTORY), "host_vars/", to_delete["server"]
             )
+
+            path_to_cleanup = os.path.join(
+                os.path.abspath(INVENTORY), "cleanup/host_vars/", to_delete["server"]
+            )
+
             if not os.path.exists(path):
                 print("Folder do no exists: {}".format(path))
                 exit(1)
 
             generated_file = os.path.join(path, "generated.yaml")
+
+            deleted_clients_file = os.path.join(path_to_cleanup, "deleted_clients.yaml")
+            if not os.path.exists(deleted_clients_file):
+                open(deleted_clients_file, 'a').close()
+
+            # Read the deleted clients yaml file
+            with open(deleted_clients_file, "r") as deleted_clients_stream:
+                deleted_clients_yaml = yaml.load(deleted_clients_stream, Loader=yaml.FullLoader)
+                if deleted_clients_yaml is None:
+                    deleted_clients_yaml = {}
+                    deleted_clients_yaml["clients"] = {}
 
             with open(generated_file, "r") as stream:
                 ymldata = yaml.load(stream, Loader=yaml.FullLoader)
@@ -47,6 +63,23 @@ def run():
                 client_name = to_delete["proxy_alias"]
                 try:
                     del client_list[client_name]
+
+                    if client_name not in deleted_clients_yaml["clients"]:
+                        # Add the client to the deleted list.
+                        deleted_clients = {}
+                        deleted_clients["clients"] = deleted_clients_yaml["clients"]
+                        deleted_clients["clients"][client_name] = {}
+                        deleted_clients["clients"][client_name]["name"] = client_name
+                        deleted_clients["clients"][client_name]["isProcessed"] = False
+
+                        deleted_clients_yaml.update(deleted_clients)
+
+                        with open(deleted_clients_file, "w") as stream:
+                            try:
+                                yaml.dump(deleted_clients_yaml, stream)
+                            except yaml.YAMLError as exc:
+                                print(exc)
+
                 except Exception:
                     pass
 
